e44a0cff66e63e91e0f331208bb0311987276cbe
rikels <knakel2@gmail.com>
2016-09-14 19:13:04 +0200

    updated to latest version
    last time I didn't commit the flaskapp?

--- flaskapp.py
+++ flaskapp.py
@@ -5,11 +5,25 @@ import json
 
 app = Flask(__name__)
 
-@app.route('/')
 @app.route('/api/course/<path:ID>', methods=['GET'])
-def nietindex(ID):
-	
-	return("{data}".format(data=json.dumps(smmbapi.smmbapi(ID))))
+def GetCourseByID(ID):
+        return("{data}".format(data=json.dumps(smmbapi.GetCourseByID(ID))))
+
+@app.route('/api/course/recommended', methods=['GET'])
+def GetRecommendedCourses():
+        return("{data}".format(data=json.dumps(smmbapi.GetRecommendedCourses())))
+
+@app.route('/api/course/ranked', methods=['GET'])
+def GetRankedCourses():
+        PageNum = int(request.args['pagenum'])
+        Type = request.args['type']
+        return("{data}".format(data=json.dumps(smmbapi.GetRankedCourses(PageNum=PageNum,Type=Type))))
+
+@app.route('/api/maker/ranked', methods=['GET'])
+def GetRankedMakers():
+        PageNum = int(request.args['pagenum'])
+        Type = request.args['type']
+        return("{data}".format(data=json.dumps(smmbapi.GetRankedMakers(PageNum=PageNum,Type=Type))))
 
 if __name__ == '__main__':
 	app.run(host="0.0.0.0")
